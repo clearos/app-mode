e8110d84575e0a4fa36efdfeb43b156208286ac1
clearostranslations <translations@clearfoundation.com>
2014-06-15 09:03:57 -0400

    Prepped for translation system

--- language/en_US/mode_lang.php
+++ language/en_US/mode_lang.php
@@ -2,9 +2,9 @@
 
 $lang['mode_app_description'] = 'The Mode app provides a low-level driver system for master/slave mode.';
 $lang['mode_app_name'] = 'Mode Manager';
-$lang['mode_mode'] = 'Mode';
 $lang['mode_master'] = 'Master';
+$lang['mode_mode_is_invalid'] = 'Mode is invalid.';
+$lang['mode_mode'] = 'Mode';
 $lang['mode_slave'] = 'Slave';
 $lang['mode_standalone'] = 'Standalone';
-$lang['mode_mode_is_invalid'] = 'Mode is invalid.';
 $lang['mode_system_mode_driver_not_set'] = 'System mode driver is not set.';
