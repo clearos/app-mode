6a2f6cf9ca3a1050b089316d6f0ddd789dd36980
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:32:46 +0000

    Add new files and directories and commit changes to local repository

--- language/no/mode_lang.php
+++ language/no/mode_lang.php
@@ -1,10 +1,10 @@
 <?php
 
-$lang['mode_app_description'] = 'The Mode app provides a low-level driver system for master/slave mode.';
-$lang['mode_app_name'] = 'Mode Manager';
-$lang['mode_master'] = 'Master';
-$lang['mode_mode_is_invalid'] = 'Mode is invalid.';
-$lang['mode_mode'] = 'Mode';
+$lang['mode_app_description'] = 'Modus app gir et lavt nivå driver system for master / slave modus.';
+$lang['mode_app_name'] = 'Mode manager';
+$lang['mode_master'] = 'Herre';
+$lang['mode_mode_is_invalid'] = 'Mode er ugyldig.';
+$lang['mode_mode'] = 'Modus';
 $lang['mode_slave'] = 'Slave';
-$lang['mode_standalone'] = 'Standalone';
-$lang['mode_system_mode_driver_not_set'] = 'System mode driver is not set.';
+$lang['mode_standalone'] = 'Frittstående stående~~POS=HEADCOMP';
+$lang['mode_system_mode_driver_not_set'] = 'System måte sjåfør er ikke satt.';
