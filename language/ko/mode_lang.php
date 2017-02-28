92d917bc2f394995fd144730c59b51afc5144344
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:30:48 +0000

    Add new files and directories and commit changes to local repository

--- language/ko/mode_lang.php
+++ language/ko/mode_lang.php
@@ -1,10 +1,10 @@
 <?php
 
-$lang['mode_app_description'] = 'The Mode app provides a low-level driver system for master/slave mode.';
-$lang['mode_app_name'] = 'Mode Manager';
-$lang['mode_master'] = 'Master';
-$lang['mode_mode_is_invalid'] = 'Mode is invalid.';
-$lang['mode_mode'] = 'Mode';
-$lang['mode_slave'] = 'Slave';
-$lang['mode_standalone'] = 'Standalone';
-$lang['mode_system_mode_driver_not_set'] = 'System mode driver is not set.';
+$lang['mode_app_description'] = '모드 앱 마스터 / 슬레이브 모드 로우 레벨 구동 시스템을 제공한다.';
+$lang['mode_app_name'] = '모드 관리자';
+$lang['mode_master'] = '석사';
+$lang['mode_mode_is_invalid'] = '모드가 잘못되었습니다.';
+$lang['mode_mode'] = '방법';
+$lang['mode_slave'] = '노예';
+$lang['mode_standalone'] = '독립';
+$lang['mode_system_mode_driver_not_set'] = '시스템 모드 드라이버가 설정되어 있지 않습니다.';
