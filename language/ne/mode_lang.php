1f7c6a63bb7d0e212f2a526baf3cab998a1a8023
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:32:30 +0000

    Add new files and directories and commit changes to local repository

--- language/ne/mode_lang.php
+++ language/ne/mode_lang.php
@@ -1,10 +1,10 @@
 <?php
 
-$lang['mode_app_description'] = 'The Mode app provides a low-level driver system for master/slave mode.';
-$lang['mode_app_name'] = 'Mode Manager';
-$lang['mode_master'] = 'Master';
-$lang['mode_mode_is_invalid'] = 'Mode is invalid.';
-$lang['mode_mode'] = 'Mode';
-$lang['mode_slave'] = 'Slave';
-$lang['mode_standalone'] = 'Standalone';
-$lang['mode_system_mode_driver_not_set'] = 'System mode driver is not set.';
+$lang['mode_app_description'] = 'मोड अनुप्रयोग मास्टर / दास मोड लागि एक कम-स्तर चालक प्रणाली प्रदान गर्दछ।';
+$lang['mode_app_name'] = 'मोड प्रबन्धक';
+$lang['mode_master'] = 'मास्टर';
+$lang['mode_mode_is_invalid'] = 'मोड अमान्य छ।';
+$lang['mode_mode'] = 'मोड';
+$lang['mode_slave'] = 'दास';
+$lang['mode_standalone'] = 'स्ट्यान्ड';
+$lang['mode_system_mode_driver_not_set'] = 'सिस्टम मोड चालक तय भएको छैन।';
