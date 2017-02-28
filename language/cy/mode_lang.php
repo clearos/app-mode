fc086e558173676da65d74b8e71ce6748e9a35a7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 21:07:39 +0000

    Add new files and directories and commit changes to local repository

--- language/cy/mode_lang.php
+++ language/cy/mode_lang.php
@@ -1,10 +1,10 @@
 <?php
 
-$lang['mode_app_description'] = 'The Mode app provides a low-level driver system for master/slave mode.';
-$lang['mode_app_name'] = 'Mode Manager';
-$lang['mode_master'] = 'Master';
-$lang['mode_mode_is_invalid'] = 'Mode is invalid.';
-$lang['mode_mode'] = 'Mode';
+$lang['mode_app_description'] = 'Mae&#39;r app Modd yn darparu system gyrrwr lefel isel ar gyfer y modd meistr / caethwas.';
+$lang['mode_app_name'] = 'Rheolwr modd';
+$lang['mode_master'] = 'Meistr';
+$lang['mode_mode_is_invalid'] = 'Modd yn annilys.';
+$lang['mode_mode'] = 'modd';
 $lang['mode_slave'] = 'Slave';
 $lang['mode_standalone'] = 'Standalone';
-$lang['mode_system_mode_driver_not_set'] = 'System mode driver is not set.';
+$lang['mode_system_mode_driver_not_set'] = 'Nid yw gyrrwr Modd system yn cael ei osod.';
