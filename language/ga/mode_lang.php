f0654bd14d7f1ed1ccb522b1db296d588048383d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:29:37 +0000

    Add new files and directories and commit changes to local repository

--- language/ga/mode_lang.php
+++ language/ga/mode_lang.php
@@ -1,10 +1,10 @@
 <?php
 
-$lang['mode_app_description'] = 'The Mode app provides a low-level driver system for master/slave mode.';
-$lang['mode_app_name'] = 'Mode Manager';
-$lang['mode_master'] = 'Master';
-$lang['mode_mode_is_invalid'] = 'Mode is invalid.';
-$lang['mode_mode'] = 'Mode';
-$lang['mode_slave'] = 'Slave';
-$lang['mode_standalone'] = 'Standalone';
-$lang['mode_system_mode_driver_not_set'] = 'System mode driver is not set.';
+$lang['mode_app_description'] = 'Soláthraíonn an app Mód córas tiománaí íseal-leibhéal do mód máistir / daor.';
+$lang['mode_app_name'] = 'Bainisteoir mód';
+$lang['mode_master'] = 'máistir';
+$lang['mode_mode_is_invalid'] = 'Is modh neamhbhailí.';
+$lang['mode_mode'] = 'mód';
+$lang['mode_slave'] = 'daor';
+$lang['mode_standalone'] = 'standalone';
+$lang['mode_system_mode_driver_not_set'] = 'Níl tiománaí modh córas a leagan síos.';
