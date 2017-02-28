fad03ccb1256fb24fb46c1068816e2de6ce44d06
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:06:32 +0000

    Add new files and directories and commit changes to local repository

--- language/sw/mode_lang.php
+++ language/sw/mode_lang.php
@@ -1,10 +1,10 @@
 <?php
 
 $lang['mode_app_description'] = 'The Mode app provides a low-level driver system for master/slave mode.';
-$lang['mode_app_name'] = 'Mode Manager';
-$lang['mode_master'] = 'Master';
-$lang['mode_mode_is_invalid'] = 'Mode is invalid.';
-$lang['mode_mode'] = 'Mode';
-$lang['mode_slave'] = 'Slave';
+$lang['mode_app_name'] = 'Meneja mode';
+$lang['mode_master'] = 'Mwalimu';
+$lang['mode_mode_is_invalid'] = 'Mode ni batili.';
+$lang['mode_mode'] = 'mode';
+$lang['mode_slave'] = 'mtumwa';
 $lang['mode_standalone'] = 'Standalone';
-$lang['mode_system_mode_driver_not_set'] = 'System mode driver is not set.';
+$lang['mode_system_mode_driver_not_set'] = 'dereva System mode si kuweka.';
