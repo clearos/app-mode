2d9212a6e579b9212dd7b7f2afbb497f7baf2f6a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:32:54 +0000

    Add new files and directories and commit changes to local repository

--- language/pa/mode_lang.php
+++ language/pa/mode_lang.php
@@ -1,10 +1,10 @@
 <?php
 
-$lang['mode_app_description'] = 'The Mode app provides a low-level driver system for master/slave mode.';
-$lang['mode_app_name'] = 'Mode Manager';
-$lang['mode_master'] = 'Master';
-$lang['mode_mode_is_invalid'] = 'Mode is invalid.';
-$lang['mode_mode'] = 'Mode';
-$lang['mode_slave'] = 'Slave';
-$lang['mode_standalone'] = 'Standalone';
-$lang['mode_system_mode_driver_not_set'] = 'System mode driver is not set.';
+$lang['mode_app_description'] = 'ਢੰਗ ਐਪਲੀਕੇਸ਼ ਨੂੰ ਮਾਸਟਰ / ਸਲੇਵ ਮੋਡ ਲਈ ਇੱਕ ਘੱਟ-ਪੱਧਰ ਡਰਾਈਵਰ ਸਿਸਟਮ ਦਿੰਦਾ ਹੈ.';
+$lang['mode_app_name'] = 'ਮੋਡ ਮੈਨੇਜਰ';
+$lang['mode_master'] = 'ਮਾਸਟਰ';
+$lang['mode_mode_is_invalid'] = 'ਮੋਡ ਗਲਤ ਹੈ.';
+$lang['mode_mode'] = 'ਮੋਡ';
+$lang['mode_slave'] = 'ਨੌਕਰ';
+$lang['mode_standalone'] = 'ਇੱਕਲਾ';
+$lang['mode_system_mode_driver_not_set'] = 'ਸਿਸਟਮ ਮੋਡ ਡਰਾਈਵਰ ਨੂੰ ਸੈੱਟ ਨਾ ਕੀਤਾ ਗਿਆ ਹੈ.';
