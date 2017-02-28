2c477b275439d80455312db1d16176043aeeee49
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:27:30 +0000

    Add new files and directories and commit changes to local repository

--- language/zh/mode_lang.php
+++ language/zh/mode_lang.php
@@ -1,10 +1,10 @@
 <?php
 
-$lang['mode_app_description'] = 'The Mode app provides a low-level driver system for master/slave mode.';
-$lang['mode_app_name'] = 'Mode Manager';
-$lang['mode_master'] = 'Master';
-$lang['mode_mode_is_invalid'] = 'Mode is invalid.';
-$lang['mode_mode'] = 'Mode';
-$lang['mode_slave'] = 'Slave';
-$lang['mode_standalone'] = 'Standalone';
-$lang['mode_system_mode_driver_not_set'] = 'System mode driver is not set.';
+$lang['mode_app_description'] = '该模式的应用程序提供了一个低级别的驱动系统，主/从模式。';
+$lang['mode_app_name'] = '模式管理器';
+$lang['mode_master'] = '主';
+$lang['mode_mode_is_invalid'] = '模式是无效的。';
+$lang['mode_mode'] = '模式';
+$lang['mode_slave'] = '奴隶';
+$lang['mode_standalone'] = '独立';
+$lang['mode_system_mode_driver_not_set'] = '系统模式驱动程序未设置。';
