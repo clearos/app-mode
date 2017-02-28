f1c1d658d25769be190f9cf6fa0c6021109501e7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:27:45 +0000

    Add new files and directories and commit changes to local repository

--- language/cs/mode_lang.php
+++ language/cs/mode_lang.php
@@ -1,10 +1,10 @@
 <?php
 
-$lang['mode_app_description'] = 'The Mode app provides a low-level driver system for master/slave mode.';
-$lang['mode_app_name'] = 'Mode Manager';
-$lang['mode_master'] = 'Master';
-$lang['mode_mode_is_invalid'] = 'Mode is invalid.';
-$lang['mode_mode'] = 'Mode';
-$lang['mode_slave'] = 'Slave';
-$lang['mode_standalone'] = 'Standalone';
-$lang['mode_system_mode_driver_not_set'] = 'System mode driver is not set.';
+$lang['mode_app_description'] = 'Aplikace režim poskytuje systém řidiče nízkoúrovňové režimu master / slave.';
+$lang['mode_app_name'] = 'Manažerský mód';
+$lang['mode_master'] = 'Mistr';
+$lang['mode_mode_is_invalid'] = 'Režim je neplatný.';
+$lang['mode_mode'] = 'režim';
+$lang['mode_slave'] = 'Otrok';
+$lang['mode_standalone'] = 'Samostatný';
+$lang['mode_system_mode_driver_not_set'] = 'Ovladač režimu systém není nastaven.';
