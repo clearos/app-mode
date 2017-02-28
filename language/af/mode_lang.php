5ddae03b1ff70aaa447f76bb558e7b7fbaadfa71
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:25:55 +0000

    Add new files and directories and commit changes to local repository

--- language/af/mode_lang.php
+++ language/af/mode_lang.php
@@ -1,10 +1,10 @@
 <?php
 
-$lang['mode_app_description'] = 'The Mode app provides a low-level driver system for master/slave mode.';
-$lang['mode_app_name'] = 'Mode Manager';
-$lang['mode_master'] = 'Master';
-$lang['mode_mode_is_invalid'] = 'Mode is invalid.';
-$lang['mode_mode'] = 'Mode';
-$lang['mode_slave'] = 'Slave';
+$lang['mode_app_description'] = 'Die modus app bied &#39;n lae-vlak bestuurder stelsel vir meester / slaaf af.';
+$lang['mode_app_name'] = 'af Bestuurder';
+$lang['mode_master'] = 'Meester';
+$lang['mode_mode_is_invalid'] = 'Modus is ongeldig.';
+$lang['mode_mode'] = 'Wyse';
+$lang['mode_slave'] = 'slaaf';
 $lang['mode_standalone'] = 'Standalone';
-$lang['mode_system_mode_driver_not_set'] = 'System mode driver is not set.';
+$lang['mode_system_mode_driver_not_set'] = 'Stelsel af bestuurder is nie ingestel.';
