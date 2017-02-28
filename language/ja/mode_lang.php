a504f94bfadd5f8ca3cd62cefb122065c0cd3a88
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:30:10 +0000

    Add new files and directories and commit changes to local repository

--- language/ja/mode_lang.php
+++ language/ja/mode_lang.php
@@ -1,10 +1,10 @@
 <?php
 
-$lang['mode_app_description'] = 'The Mode app provides a low-level driver system for master/slave mode.';
-$lang['mode_app_name'] = 'Mode Manager';
-$lang['mode_master'] = 'Master';
-$lang['mode_mode_is_invalid'] = 'Mode is invalid.';
-$lang['mode_mode'] = 'Mode';
-$lang['mode_slave'] = 'Slave';
-$lang['mode_standalone'] = 'Standalone';
-$lang['mode_system_mode_driver_not_set'] = 'System mode driver is not set.';
+$lang['mode_app_description'] = 'モードアプリは、マスター/スレーブモード用の低レベルのドライバシステムを提供します。';
+$lang['mode_app_name'] = 'モードマネージャ';
+$lang['mode_master'] = 'マスター';
+$lang['mode_mode_is_invalid'] = 'モードは無効です。';
+$lang['mode_mode'] = 'モード';
+$lang['mode_slave'] = 'スレーブ';
+$lang['mode_standalone'] = 'スタンドアロン';
+$lang['mode_system_mode_driver_not_set'] = 'システムモードドライバが設定されていません。';
