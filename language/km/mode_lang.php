7ef19474d834eed2bcc75ba293077fa0ab01d777
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:30:32 +0000

    Add new files and directories and commit changes to local repository

--- language/km/mode_lang.php
+++ language/km/mode_lang.php
@@ -1,10 +1,10 @@
 <?php
 
-$lang['mode_app_description'] = 'The Mode app provides a low-level driver system for master/slave mode.';
-$lang['mode_app_name'] = 'Mode Manager';
-$lang['mode_master'] = 'Master';
-$lang['mode_mode_is_invalid'] = 'Mode is invalid.';
-$lang['mode_mode'] = 'Mode';
-$lang['mode_slave'] = 'Slave';
-$lang['mode_standalone'] = 'Standalone';
-$lang['mode_system_mode_driver_not_set'] = 'System mode driver is not set.';
+$lang['mode_app_description'] = 'កម្មវិធីរបៀបនេះបានផ្តល់នូវប្រព័ន្ធកម្មវិធីបញ្ជាកម្រិតទាបសម្រាប់របៀបម្ចាស់ / ទាសករ។';
+$lang['mode_app_name'] = 'កម្មវិធីគ្រប់គ្រងរបៀប';
+$lang['mode_master'] = 'អនុបណ្ឌិត';
+$lang['mode_mode_is_invalid'] = 'របៀបមិនត្រឹមត្រូវ។';
+$lang['mode_mode'] = 'របៀប';
+$lang['mode_slave'] = 'ខ្ញុំបម្រើ';
+$lang['mode_standalone'] = 'ឯង';
+$lang['mode_system_mode_driver_not_set'] = 'កម្មវិធីបញ្ជារបៀបប្រព័ន្ធមិនត្រូវបានកំណត់។';
