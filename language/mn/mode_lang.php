76e4de31953af96af7a28b51dbe625d1b849878b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:31:58 +0000

    Add new files and directories and commit changes to local repository

--- language/mn/mode_lang.php
+++ language/mn/mode_lang.php
@@ -1,10 +1,10 @@
 <?php
 
-$lang['mode_app_description'] = 'The Mode app provides a low-level driver system for master/slave mode.';
-$lang['mode_app_name'] = 'Mode Manager';
-$lang['mode_master'] = 'Master';
-$lang['mode_mode_is_invalid'] = 'Mode is invalid.';
-$lang['mode_mode'] = 'Mode';
-$lang['mode_slave'] = 'Slave';
-$lang['mode_standalone'] = 'Standalone';
-$lang['mode_system_mode_driver_not_set'] = 'System mode driver is not set.';
+$lang['mode_app_description'] = 'Горим апп мастер / боол горимд нь бага түвшний жолооч системтэй байдаг.';
+$lang['mode_app_name'] = 'Горим менежер';
+$lang['mode_master'] = 'мастер';
+$lang['mode_mode_is_invalid'] = 'Горим хүчин төгөлдөр бус байна.';
+$lang['mode_mode'] = 'Горим';
+$lang['mode_slave'] = 'боол';
+$lang['mode_standalone'] = 'дан';
+$lang['mode_system_mode_driver_not_set'] = 'Системийн горим драйвер тогтоосон байна.';
