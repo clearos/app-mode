7bd8289046b2f93241bf1c006b35cbc567c2bf03
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 22:27:16 +0000

    Add new files and directories and commit changes to local repository

--- language/ca/mode_lang.php
+++ language/ca/mode_lang.php
@@ -1,10 +1,10 @@
 <?php
 
-$lang['mode_app_description'] = 'The Mode app provides a low-level driver system for master/slave mode.';
-$lang['mode_app_name'] = 'Mode Manager';
-$lang['mode_master'] = 'Master';
-$lang['mode_mode_is_invalid'] = 'Mode is invalid.';
-$lang['mode_mode'] = 'Mode';
-$lang['mode_slave'] = 'Slave';
-$lang['mode_standalone'] = 'Standalone';
-$lang['mode_system_mode_driver_not_set'] = 'System mode driver is not set.';
+$lang['mode_app_description'] = 'L&#39;aplicació de manera proporciona un sistema controlador de baix nivell per al funcionament mestre / esclau.';
+$lang['mode_app_name'] = 'Administrador de manera de';
+$lang['mode_master'] = 'mestre';
+$lang['mode_mode_is_invalid'] = 'Manera no és vàlid.';
+$lang['mode_mode'] = 'manera';
+$lang['mode_slave'] = 'esclau';
+$lang['mode_standalone'] = 'autònom';
+$lang['mode_system_mode_driver_not_set'] = 'controlador de manera de sistema no està configurat.';
